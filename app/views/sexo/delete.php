3cc45e1651206469041c6adcf30f69f39bfca3a1
Paul18-L <paul20020818@gmail.com>
2025-05-20 15:20:13 -0500

    Update delete.php

--- app/views/sexo/delete.php
+++ app/views/sexo/delete.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elininar Sexo</title>
+    <title>Eliminar Sexo</title>
 </head>
 <body>
 
