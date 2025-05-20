bc7d5b98837bf8bfbef576a4038b266590d74c42
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-20 15:04:20 -0500

    Update create.php

--- app/views/direccion/create.php
+++ app/views/direccion/create.php
@@ -1,5 +1,16 @@
-<form action="../../controllers/DireccionController.php?action=create" method="POST">
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" required>
+ <form action="../../controllers/DireccionController.php?action=create" method="POST">
+    <label for="idpersona">Persona:</label>
+    <select name="idpersona" id="idpersona" required>
+        <option value="">Seleccione una persona</option>
+        <?php foreach ($personas as $persona): ?>
+            <option value="<?= htmlspecialchars($persona['idpersona']) ?>">
+                <?= htmlspecialchars($persona['nombre']) ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+
+    <label for="nombre">Dirección:</label>
+    <input type="text" name="nombre" id="nombre" required placeholder="Ej. Av. Central 123">
+
     <input type="submit" value="Crear">
-</form> 
+ </form>
