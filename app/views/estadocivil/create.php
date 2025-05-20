f2c36e7ddc18a16c550a675c62848c1ff5b97157
emelyjiron <emelyjiron16@gmail.com>
2025-05-20 17:02:00 -0500

    Update create.php

--- app/views/estadocivil/create.php
+++ app/views/estadocivil/create.php
@@ -1,5 +1,5 @@
 <form action="../../controllers/EstadocivilController.php?action=create" method="POST">
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" required>
+    <input type="text" name="nombre" id="nombre" required maxlength="50" placeholder="Ingrese el nombre del estado civil">
     <input type="submit" value="Crear Estado Civil">
 </form>
