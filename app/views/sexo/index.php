4122b34dd2cc5c26d01a117b768f710bc66433f7
Paul18-L <paul20020818@gmail.com>
2025-05-20 15:29:54 -0500

    Update index.php

--- app/views/sexo/index.php
+++ app/views/sexo/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar Sexos</title>
-    <link rel="stylesheet" href="/apple6b/public/css/style.css">
+    <link rel="stylesheet" href="/ibm5a/public/css/style.css">
 </head>
 <body>
 
