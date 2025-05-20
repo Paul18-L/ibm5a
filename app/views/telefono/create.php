4674c9a5b3da87f947578f71765b63f224ba25d8
emelyjiron <emelyjiron16@gmail.com>
2025-05-20 17:43:39 -0500

    Update create.php

--- app/views/telefono/create.php
+++ app/views/telefono/create.php
@@ -1,4 +1,4 @@
-<form action="../../controllers/SexoController.php?action=create" method="POST">
+<form action="../../controllers/telefonoController.php?action=create" method="POST">
     <label for="nombre">Nombre:</label>
     <input type="text" name="nombre" id="nombre" required>
     <input type="submit" value="Crear">
