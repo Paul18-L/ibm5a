a9ec96d9cae746be997de2da622778f27994b6ff
Paul18-L <paul20020818@gmail.com>
2025-05-20 14:54:27 -0500

    Update delete.php

--- app/views/sexo/delete.php
+++ app/views/sexo/delete.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elininar Sexo</title>
+    <title>Eliminar Sexo</title>
 </head>
 <body>
 
 <h1>Editar el Sexo</h1>
-<form action="/apple5a/public/sexo/delete" method="POST">
+<form action="/ibm5a/public/sexo/delete" method="POST">
     <input type="hidden" name="idsexo" value="<?php echo htmlspecialchars($sexo['idsexo']); ?>">
     
     <label for="nombre">Nombre:</label>
