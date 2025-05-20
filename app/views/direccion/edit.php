185892b7966d24bbbb3f8e4b886a6db314b4691f
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-20 15:02:57 -0500

    Update edit.php

--- app/views/direccion/edit.php
+++ app/views/direccion/edit.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Sexo</title>
+    <title>Editar dirección</title>
 </head>
 <body>
 
-<h1>Editar </h1>
-<form action="/apple5a/public/sexo/update" method="POST">
-    <input type="hidden" name="idsexo" value="<?php echo htmlspecialchars($sexo['idsexo']); ?>">
-    
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($sexo['nombre']); ?>" required>
-    
+<h1>Editar Dirección</h1>
+
+<form action="/ibm5a/public/direccion/update" method="POST">
+    <input type="hidden" name="iddireccion" value="<?php echo htmlspecialchars($direccion['iddireccion']); ?>">
+
+    <label for="nombre">Dirección:</label>
+    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($direccion['nombre']); ?>" required>
+
     <input type="submit" value="Actualizar">
 </form>
 
-<a href="index">Volver al listado</a>
+<a href="/ibm5a/public/direccion/index">Volver al listado</a>
 
 </body>
 </html>
