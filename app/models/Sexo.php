733d55669e2261b5fd51ef3368e5d9b6e6f4bf3d
Paul18-L <paul20020818@gmail.com>
2025-05-20 15:35:28 -0500

    Update Sexo.php

--- app/models/Sexo.php
+++ app/models/Sexo.php
@@ -1,9 +1,9 @@
 <?php
-
+//controlador sexo
 class Sexo {
     private $conn;
     private $table_name = "sexo";
-// modificado por programador martinez casanova carlos andres
+
     public $id;
     public $nombre;
 
@@ -11,7 +11,7 @@ class Sexo {
         $this->conn = $db;
     }
 
-    
+    // Crear un nuevo sexo
     public function create() {
         try {
             $query = "INSERT INTO " . $this->table_name . " (nombre) VALUES (:nombre)";
@@ -23,6 +23,13 @@ class Sexo {
             error_log("Error en create(): " . $e->getMessage());
             return false;
         }
+    }
+
+
+ public function getAll() {
+        // Conexión a la base de datos
+        $query = $this->conn->query("SELECT idsexo, nombre FROM sexo");
+        return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 
 
@@ -75,22 +82,19 @@ public function read() {
     // Eliminar un sexo
     public function delete() {
         try {
-            if (empty($this->id)) {
+            if (empty($this->idsexo)) {
                 return false;
             }
-	            error_log("Intentando eliminar el ID: " . $this->id);
-
-
-
+	         error_log("Intentando eliminar el ID: " . $this->idsexo);
 
 	  // Preparar la consulta
-        $query = "DELETE FROM " . $this->table_name . " WHERE id = :id";
+        $query = "DELETE FROM " . $this->table_name . " WHERE idsexo = :idsexo";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":id", $this->id, PDO::PARAM_INT);
+        $stmt->bindParam(":idsexo", $this->idsexo, PDO::PARAM_INT);
 	
 // Ejecutar la consulta
         if ($stmt->execute()) {
-            error_log("Registro con ID " . $this->id . " eliminado correctamente.");
+            error_log("Registro con ID " . $this->idsexo . " eliminado correctamente.");
             return true;
         } else {
             error_log("Error en delete(): La consulta no se ejecutó correctamente.");
