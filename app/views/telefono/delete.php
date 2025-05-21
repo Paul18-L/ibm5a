d10bad04bfb4fac291e922cb3a3571aebcea4f7d
emelyjiron <emelyjiron16@gmail.com>
2025-05-21 18:00:00 -0500

    Update delete.php

--- app/views/telefono/delete.php
+++ app/views/telefono/delete.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elininar Sexo</title>
+    <title>Eliminar Teléfono</title>
 </head>
 <body>
 
-<h1>Editar el Sexo</h1>
-<form action="/apple5a/public/sexo/delete" method="POST">
-    <input type="hidden" name="idsexo" value="<?php echo htmlspecialchars($sexo['idsexo']); ?>">
+<h1>Eliminar el Teléfono</h1>
+<form action="/ibm5a/public/telefono/delete" method="POST">
+    <input type="hidden" name="idtelefono" value="<?php echo htmlspecialchars($telefono['idtelefono']); ?>">
     
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($sexo['nombre']); ?>" required>
+    <label for="numero">Número:</label>
+    <input type="text" name="numero" id="numero" value="<?php echo htmlspecialchars($telefono['numero']); ?>" required>
     
     <input type="submit" value="Eliminar">
 </form>
