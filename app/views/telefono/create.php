aced414bfb1422dd8f1088259b3e260924cc5cc6
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-21 18:11:29 -0500

    Update create.php

--- app/views/telefono/create.php
+++ app/views/telefono/create.php
@@ -1,5 +1,13 @@
-<form action="../../controllers/telefonoController.php?action=create" method="POST">
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" required>
-    <input type="submit" value="Crear">
+<h1>Agregar Teléfono</h1>
+
+<form action="/ibm5a/public/telefono/store" method="POST">
+    <label for="idpersona">ID de Persona:</label>
+    <input type="number" name="idpersona" id="idpersona" required>
+
+    <label for="numero">Número de Teléfono:</label>
+    <input type="text" name="numero" id="numero" required>
+
+    <input type="submit" value="Guardar">
 </form>
+
+<a href="/ibm5a/public/telefono/index">Volver al listado</a>
