af01a938bfa2e9857f1e2fdb3d2aaaf185d457cd
NicoleOrtiz <125750299+Rkjrrjf292@users.noreply.github.com>
2025-05-22 12:42:07 -0500

    Update index.php

--- app/views/telefono/index.php
+++ app/views/telefono/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-// lista Teléfono 
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar Teléfonos</title>
