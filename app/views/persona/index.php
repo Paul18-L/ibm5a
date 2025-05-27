7fa2b008cbd56354773af1f2fd657d7dd354b9a1
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-27 17:57:46 -0500

    Update index.php

--- app/views/persona/index.php
+++ app/views/persona/index.php
@@ -32,8 +32,8 @@
                         <td><?php echo htmlspecialchars($persona['nombres']); ?></td>
                         <td><?php echo htmlspecialchars($persona['apellidos']); ?></td>
                         <td><?php echo htmlspecialchars($persona['fechanacimiento']); ?></td>
-                        <td><?php echo htmlspecialchars($persona['elsexo']); ?></td>
-                        <td><?php echo htmlspecialchars($persona['elestadocivil']); ?></td>
+                        <td><?php echo htmlspecialchars($persona['sexo']); ?></td>
+                        <td><?php echo htmlspecialchars($persona['estadocivil']); ?></td>
                         <td>
                             <a href="/ibm5a/public/persona/view?idpersona=<?php echo htmlspecialchars($persona['idpersona']); ?>">
                                 <button>View</button>
