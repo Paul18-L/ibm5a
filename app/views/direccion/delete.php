4aa1b6ab2bb69f199dbfc48bec23692f291650a0
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-27 20:17:31 -0500

    Update delete.php

--- app/views/direccion/delete.php
+++ app/views/direccion/delete.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eliminar dirrecion</title>
+    <title>Eliminar dirección</title>
 </head>
 <body>
 
-<h1>Editar dirrecion</h1>
-<form action="/ibm5a/public/dirreción/delete" method="POST">
-    <input type="hidden" name="iddirrecion" value="<?php echo htmlspecialchars($sexo['iddirrecion']); ?>">
+<h1>Editar dirección</h1>
+<form action="/ibm5a/public/dirección/delete" method="POST">
+    <input type="hidden" name="iddireccion" value="<?php echo htmlspecialchars($direccion['iddireccion']); ?>">
     
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($sexo['nombre']); ?>" required>
+    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($direccion['nombre']); ?>" required>
     
     <input type="submit" value="Eliminar">
 </form>
