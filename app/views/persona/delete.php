acca3fbcf4e1815196b214b38c2d13f996c04ada
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-05-31 10:25:49 -0500

    Update delete.php

--- app/views/persona/delete.php
+++ app/views/persona/delete.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eliminar Sexo</title>
+    <title>Eliminar Persona</title>
 </head>
 <body>
 
-<h1>Eliminar el Sexo</h1>
-<form action="/ibm5a/public/sexo/delete" method="POST">
-    <input type="hidden" name="idsexo" value="<?php echo htmlspecialchars($sexo['idsexo']); ?>">
+<h1>Eliminar la Persona</h1>
+<form action="/ibm5a/public/persona/delete" method="POST">
+    <input type="hidden" name="idpersona" value="<?php echo htmlspecialchars($persona['idpersona']); ?>">
     
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($sexo['nombre']); ?>" required readonly>
+    <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($persona['nombre']); ?>" required readonly>
     
     <input type="submit" value="Eliminar">
 </form>
